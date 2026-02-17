7c8d8229e1bcdf344a969108da985d870f9b916a
Asterpo <104466737+Asterpo@users.noreply.github.com>
2026-02-16 20:49:52 -0800

    Add files via upload

---
+++ Packages/Signal.lua
@@ -0,0 +1,5 @@
+local REQUIRED_MODULE = require(script.Parent._Index["lucasmzreal_fastsignal@10.4.0"]["fastsignal"])
+export type ScriptSignal<T...> = REQUIRED_MODULE.ScriptSignal<T...>
+export type ScriptConnection = REQUIRED_MODULE.ScriptConnection 
+export type Class = REQUIRED_MODULE.Class 
+return REQUIRED_MODULE
